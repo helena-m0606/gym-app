97c670ea3be292d64b5ff4f065de79afd3b2c29a
helena-m0606 <helenamondriguez@gmail.com>
2026-05-13 17:05:30 -0600

    Logo y nombre UPDATED

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])>
     <head>
         <meta charset="utf-8">
+       <link rel="shortcut icon" href="{{ asset('TitanGYM.png') }}?v=2">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
@@ -31,17 +32,13 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         <x-inertia::head>
-            <title>{{ config('app.name', 'Laravel') }}</title>
+            <title>{{ config('app.name', 'TitanGYM') }}</title>
         </x-inertia::head>
     </head>
     <body class="font-sans antialiased">
